2682ab5d0d58bcd2faa0067fe04c647e615def74
iRpro16 <rumeoisidoro16@gmail.com>
2025-03-28 17:48:26 -0400

    styling

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app  = express();
 const path = require("node:path");
 const indexRouter = require("./routers/indexRouter");
+const assetsPath = path.join(__dirname, "public");
 
 // setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(assetsPath));
 
 
 app.use("/", indexRouter);
